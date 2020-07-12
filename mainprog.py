2f5ce840127dff39c366c3a4c8ea49bd50bd5dbd
Henry Cafaro <67771343+hjcafaroUC@users.noreply.github.com>
2020-07-12 13:06:34 -0600

    Add files via upload

--- mainprog.py
+++ mainprog.py
@@ -4,25 +4,44 @@ import sys; print(sys.version)
 
 print("What is the dimensionality of your problem?")
 
-dim = int(input())
+dpdim = int(input())
 
-dims = []
+dpdims = []
 print("Enter the maximum bounds on each dimension:")
 #global multidimensional array will be allocated with these bounds
 
-for i in range(dim):
-    dims.append(int(input()))
+for i in range(dpdim):
+    dpdims.append(int(input()))
+
+datadim = 0
+datadims = []
+print("Enter the dimensionality of your data")
+s = input()
+if(s == "sm"):
+    datadim = dpdim
+    datadims = dpdims[::]
+else:
+    datadim = int(s)
+    print("Enter the maximum bounds on each dimension of data:")
+    # data array will be allocated with these dimensions
+
+    for i in range(datadim):
+        datadims.append(int(input()))
+
 
 dpPlacehold= "dp"
 fPlacehold = "f("
+aPlacehold = "a"
 
+for i in range(1,datadim+1):
+    aPlacehold = aPlacehold + "[x" + str(i) + "]"
 
-for i in range(1,dim+1):
+for i in range(1,dpdim+1):
     dpPlacehold = dpPlacehold + "[x" + str(i) + "]"
     fPlacehold = fPlacehold + "x" + str(i) + ","
 fPlacehold = fPlacehold[:-1] + ")"
 
-#these are useful placeholder strings: the dp one is dp[x1], or dp[x1][2], etc.
+#these are useful placeholder strings: the dp one is dp[x1], or dp[x1][x2], etc.
 # the f one is f(x1), or f(x1,x2), etc
 
 #To do: add changing variable names
@@ -58,9 +77,9 @@ if dpRuleString == "sw":
             break
 
 functionString = "ll int f("
-for i in range(1,dim):
+for i in range(1,dpdim):
     functionString = functionString + "int x" + str(i) + ","
-functionString = functionString + "int x" + str(dim) + "){\n"
+functionString = functionString + "int x" + str(dpdim) + "){\n"
 functionString = functionString + "if(" + dpPlacehold + "==-1){\n"
 
 if(len(cases) > 1):
@@ -79,28 +98,37 @@ inputType = input()
 #builds input parameters from user, can either fill a data array or just take in variables to evaluate answer at
 
 cinString = "int "
-for i in range(1,dim):
+for i in range(1,datadim):
     cinString = cinString + "y" + str(i) + ","
 
-cinString = cinString + "y" + str(dim) + ";\ncin >> "
-for i in range(1,dim):
+cinString = cinString + "y" + str(datadim) + ";\ncin >> "
+for i in range(1,datadim):
     cinString = cinString + "y" + str(i) + " >> "
-cinString = cinString + "y" + str(dim) + ";\n"
+cinString = cinString + "y" + str(datadim) + ";\n"
 
-arrCinString1 = ""
+dpSetString1 = ""
 
-arrCinString2 = ""
+dpSetString2 = ""
 
-for i in range(1,dim+1):
-    arrCinString1 = arrCinString1 + "for(int x" + str(i) + "=0; x" + str(i) + " < y" + str(i) + "; x" + str(i) + "++){\n"
+for i in range(1,dpdim+1):
+    dpSetString1 = dpSetString1 + "for(int x" + str(i) + "=0; x" + str(i) + " < y" + str(i) + "; x" + str(i) + "++){\n"
 
-    arrCinString2 = arrCinString2 + "}\n"
+    dpSetString2 = dpSetString2 + "}\n"
+
+dataCinString1 = ""
+
+dataCinString2 = ""
+
+for i in range(1,datadim+1):
+    dataCinString1 = dataCinString1 + "for(int x" + str(i) + "=0; x" + str(i) + " < y" + str(i) + "; x" + str(i) + "++){\n"
+
+    dataCinString2 = dataCinString2 + "}\n"
 
 if ((inputType == "standard") or (inputType == "st")):
 
-    totalInputString = cinString + arrCinString1 + "cin >> a" + dpPlacehold[2:] + ";\n" + dpPlacehold + "=-1;\n" + arrCinString2
+    totalInputString = cinString + dpSetString1 + dpPlacehold + "=-1;\n" + dpSetString2 + dataCinString1 + "cin >> " + aPlacehold + ";\n" + dataCinString2
 elif(inputType == "no"):
-    totalInputString = cinString + arrCinString1 + dpPlacehold + "=-1;\n" + arrCinString2
+    totalInputString = cinString + dpSetString1 + dpPlacehold + "=-1;\n" + dpSetString2
 
 #builds evaluation and answer printing statement
 #right now, can either take max of dp elements or evaluate f() at a single point
@@ -110,13 +138,13 @@ evalType = input()
 
 evalString = ""
 if(evalType == "mx"):
-    evalString = "ll int mx = 0;\n" + arrCinString1 + "mx = max(mx," + fPlacehold + ");\n" + arrCinString2 + "cout << mx << endl;\n"
+    evalString = "ll int mx = 0;\n" + dpSetString1 + "mx = max(mx," + fPlacehold + ");\n" + dpSetString2 + "cout << mx << endl;\n"
 elif(evalType == "eval"):
     evalString = "cout << f("
-    for i in range(1,dim):
+    for i in range(1,dpdim):
         print("Input variable " + str(i))
         evalString = evalString + input() + ","
-    print("Input variable " + str(dim))
+    print("Input variable " + str(dpdim))
     evalString = evalString + input() + ") << endl;\n"
 
 
@@ -126,12 +154,12 @@ mainString = "int main() {\n" + totalInputString + evalString + "}"
 
 #header includes dependencies, allocates variables 
 headerString = "//generated with autodp, Henry Cafaro \n# include <iostream>\n#define ll long long\nusing namespace std;\nll int a"
-for i in range(dim):
-    headerString = headerString + "[" + str(dims[i]) + "]"
+for i in range(datadim):
+    headerString = headerString + "[" + str(datadims[i]) + "]"
 
 headerString = headerString + ";\nll int dp"
-for i in range(dim):
-    headerString = headerString + "[" + str(dims[i]) + "]"
+for i in range(dpdim):
+    headerString = headerString + "[" + str(dpdims[i]) + "]"
 
 headerString = headerString + ";\n"
 
